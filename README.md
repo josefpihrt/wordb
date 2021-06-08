255c84be459a1baf810b9b98cfc327e9ac48da4a
Josef Pihrt <josef.pihrt@outlook.com>
2021-06-08 14:00:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Wordb
+
+* Wordb is a curated list of English words separated into several categories (~60 000 words in total).
+* Wordb is primary intended to be used for spellchecking.
+  * It contains fixes of various misspellings/typos in a format `<ERROR>: <FIX>` (for example `mispell: misspell`)
+* Wordb was originally created to be used with following tools:
+  * [Orang](https://www.nuget.org/packages/Orang.DotNet.Cli) (`orang spellcheck` command)
+  * [Roslynator CLI](https://www.nuget.org/packages/Roslynator.CommandLine) (`roslynator spellcheck` command)
---
+++ src/Wordb/Program.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Wordb
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (string filePath in Directory.EnumerateFiles(@"..\..\..\..\..\data", "*.txt", SearchOption.AllDirectories))
+                Normalize(filePath);
+
+            foreach (string filePath in Directory.EnumerateFiles(@"..\..\..\..\..\data.other", "*.txt", SearchOption.AllDirectories))
+                Normalize(filePath);
+        }
+
+        private static void Normalize(string path)
+        {
+            Console.WriteLine(path);
+
+            List<string> values = File.ReadLines(path)
+                .Where(f => !string.IsNullOrWhiteSpace(f))
+                .Select(f => f.Trim())
+                .Distinct(StringComparer.InvariantCulture)
+                .OrderBy(f => f, StringComparer.InvariantCulture)
+                .ToList();
+
+            string content = string.Join(Environment.NewLine, values);
+
+            if (!string.IsNullOrEmpty(content))
+                content += Environment.NewLine;
+
+            File.WriteAllText(path, content, Encoding.UTF8);
+        }
+    }
+}
