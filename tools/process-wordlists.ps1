6c402617434976d28f7f5e527e7ad8c62c11fb03
Josef Pihrt <josef@pihrt.net>
2023-10-02 13:03:22 +0200

    Migrate script to powershell

--- tools/process-wordlists.ps1
+++ tools/process-wordlists.ps1
@@ -1,7 +1,3 @@
-@echo off
-
 dotnet build "..\src\Wordb.CommandLine\Wordb.CommandLine.csproj" -c Release
 
 dotnet "..\src\Wordb.CommandLine\bin\Release\netcoreapp3.1\Wordb.CommandLine.dll" "../data" "../data.other"
-
-pause
